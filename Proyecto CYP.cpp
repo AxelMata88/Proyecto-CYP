167ba80ea5ff6a1ed88a946494ee6779dfefddad
Axel <143908475+AxelMata88@users.noreply.github.com>
2023-12-06 14:02:52 -0600

    Version 1

---
+++ Proyecto CYP.cpp
@@ -0,0 +1,20 @@
+/*****************************************************************************************************************
+	UNIVERSIDAD NACIONAL AUTONOMA DE MEXICO
+	FACULTAD DE ESTUDIOS SUPERIORES -ARAGON-
+
+	Computadoras y programacion.
+	(c)
+	Quiso decir: Programa principal de la aplicacion de la distancia de Levenstein.
+
+******************************************************************************************************************/
+
+
+#include "stdafx.h"
+#include <string.h>
+#include "corrector.h"
+#include <stdlib.h>
+#include <ctype.h>
+#define LONGITUD 32
+char abecedario[LONGITUD + 6] = "abcdefghijklmnñopqrstuvwxyzáéíóú";
+
+
