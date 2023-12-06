6dfd9679824fbe7e1c9e24aa79ad905c9fffea83
Axel <143908475+AxelMata88@users.noreply.github.com>
2023-12-06 14:06:57 -0600

    Version 3

--- Proyecto CYP.cpp
+++ Proyecto CYP.cpp
@@ -64,4 +64,80 @@ void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 	if (fp == NULL)
 		return;
 
+/*Mientras que la lectura sea igual al fichero (sea diferente a fin del documento */
+	while ((lectura = fgetc(fp)) != EOF) {
+
+		/*Declaramos una bandera*/
+		/*Nota: bandera = flag*/
+		bool bandera = true;
+
+		/*Para pasar las mayusculas a minusculas*/
+		lectura = tolower(lectura);
+
+		/*Comprobar que no hay palabras con signo de puntuación */
+		for (i = 0; i < strlen(puntuacion); i++)
+
+			/*Compara si el caracter que recibió si esta dentro de la lectura o es un signo de puntuación*/
+			if (puntuacion[i] == lectura)
+				bandera = false;
+
+		/*Si count es menor que TAMTOKEN Y de la bandera <<Continuar>> */
+		if (contador < TAMTOKEN && bandera) {
+			/*De lectura guardarlo al array*/
+			array[contador++] = lectura;
+			continue;
+		}
+		/* Si no(si es count el igua a cero <<Continuar>> */
+		else if (contador == 0)
+			continue;
+
+		/*Si no(si es count el igua a 1, asignar 0 a count. <<Continuar>> */
+		else if (contador == 1 && bandera) {
+			contador = 0;
+			continue;
+		}
+		/*Cuando las encuentre marcar un espacio*/
+		array[contador] = '\0';
+
+		/*Despues de comprobar en el abecedario y la puntuacion */
+		for (i = 0; i < iNumElementos && !bandera; i++) {
+
+			/*Aqui se hace la comparacion de las palabras*/
+			if (strcmp(szPalabras[i], array) == 0) {
+				/*Hacerlo hasta formar la palabra*/
+				iEstadisticas[i]++;
+				bandera = true;
+			}
+		}
+		/*Si es diferente de la bandera*/
+		if (!bandera) {
+			/*Pasar todos los palabras que estan en el array a szPalabras[]*/
+			strcpy_s(szPalabras[iNumElementos], array);
+			iEstadisticas[iNumElementos++]++;
+
+		}/*Para retornar*/
+		contador = 0;
+	}/*Fin del while o el documento*/
+	fclose(fp);
+
+	 
+	//iEstadisticas[iNumElementos] = 1;
+
+	/*Método burbuja para el ordenamiento*/
+	for (int j = 0; j < iNumElementos - 1; j++) {
+
+		for (i = j + 1; i < iNumElementos; i++) {
+
+			if (strcmp(szPalabras[j], szPalabras[i]) > 0) {
+				strcpy_s(array, szPalabras[j]);
+				strcpy_s(szPalabras[j], szPalabras[i]);
+				strcpy_s(szPalabras[i], array);
+				contador = iEstadisticas[j];
+				iEstadisticas[j] = iEstadisticas[i];
+				iEstadisticas[i] = contador;
+			}
+		}
+	}
+}
+
 
